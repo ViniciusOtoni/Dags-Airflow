7386bba272ea72f682a891766564719338d503d2
ViniciusOtoni <viniciusotonimsv@gmail.com>
2024-09-10 23:55:34 -0300

    Adicionando a Dag para processo Silver, Dag funcionando perfeitamente...

---
+++ ETL/silver/main.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import os
+import json
+
+import logging
+logging.basicConfig(level=logging.INFO)
+
+class SilverETL:
+
+    def __init__(self, path_df, output_dir, file_name):
+
+        self.df = self.readDataframe(path_df)
+        
+        self.df = self.cleaningBudget(self.df)
+        self.df = self.cleaningRevenue(self.df)
+        self.df = self.applyCastSize(self.df)
+
+        self.output_dir = output_dir
+        os.makedirs(self.output_dir, exist_ok=True)
+
+        self.save_to_csv(file_name)
+
+    def readDataframe(self, path_df):
+        try:
+            df = pd.read_csv(path_df)
+            logging.info("Leitura sucesso!")
+            return df
+        
+        except Exception as e:
+            logging.error(f"Erro na leitura!: {e}")
+            raise
+    
+    def cleaningBudget(self, df):
+
+        df["Movie_Budget"] = pd.to_numeric(df["Movie_Budget"], errors="coerce")
+        df["Movie_Budget"].fillna(0)
+        df["Movie_Budget"] = df["Movie_Budget"].div(100000)
+
+        return df
+
+    def cleaningRevenue(self, df):
+        
+        df["Movie_Revenue"] = pd.to_numeric(df["Movie_Revenue"], errors="coerce")
+        df["Movie_Revenue"].fillna(0)
+        df["Movie_Revenue"] = df["Movie_Revenue"].div(100000)
+
+        return df
+
+    
+    def applyCastSize(self, df):
+
+        df["Cast_List"] = df["Cast"].apply(lambda x: x.strip('|').split('|') if isinstance(x, str) else [])
+        df["Cast_Size"] = df["Cast_List"].apply(lambda x: len(x))
+        df.drop(columns=["Cast_List"], inplace=True)
+
+        return df
+
+    def save_to_csv(self, file_name):
+        try:
+            if not self.df.empty:
+                filepath = os.path.join(self.output_dir, file_name)
+                self.df.to_csv(filepath, index=False)
+                logging.info(f"Arquivo salvo em: {filepath} ")
+            else:
+                logging.warning("Não possui valor o Dataframe")
+
+        except Exception as e:
+            logging.error(f"Erro salvando CSV: {e}")
+            raise
--- airflow/dags/dag_silver.py
+++ airflow/dags/dag_silver.py
@@ -2,17 +2,33 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 from datetime import timedelta
+import logging
+import sys
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+sys.path.append('/opt/airflow/ETL/silver')
+
+from main import SilverETL
 
 def silver_processing():
-    # Função para processamento de dados no estágio silver
-    print("Processing Silver data...")
+    try:
+        path_df = '/opt/airflow/data/d02_bronze/movies_complete_bronze.csv'
+        output_dir = '/opt/airflow/data/d03_silver/'
+        file_name = 'movies_complete_silver.csv'
+        etl = SilverETL(path_df, output_dir, file_name)
+        logger.info("Processo ETL completo")
+    except Exception as e:
+        logger.error(f"Erro no ETL: {e}")
+        raise
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 1,
+    'retries': 0,
     'retry_delay': timedelta(minutes=5),
 }
 
@@ -20,12 +36,17 @@ with DAG(
     'silver_dag',
     default_args=default_args,
     description='DAG para processamento de dados em estágio silver',
-    schedule_interval=timedelta(days=1),
+    schedule_interval="@daily",
     start_date=days_ago(1),
     catchup=False,
 ) as dag:
+    
+  
 
     process_silver = PythonOperator(
         task_id='process_silver',
         python_callable=silver_processing,
+        provide_context=True, 
     )
+
+
